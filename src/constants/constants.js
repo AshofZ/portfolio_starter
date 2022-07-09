a55f6a9cd55ddfc907e9421798c6bd9b0dee8b03
AshofZ <ashof.z17@gmail.com>
2022-07-10 01:57:43 +0700

    first commit

---
+++ src/constants/constants.js
@@ -0,0 +1,47 @@
+export const projects = [
+  {
+    title: 'Shopiseven',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      image: '/images/1.png',
+      tags: ['Django', 'Python', 'E-Commerce'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'Landing Page',
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    image: '/images/2.png',
+    tags: ['React', 'Next Js'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'Cryptocurrency',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      image: '/images/3.png',
+      tags: ['Django', 'Python', 'RapidAPI'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Digital Invitation',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    image: '/images/4.png',
+    tags: ['Html', 'Invitation'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Lorem Ipsum is simply dummy text', },
+  { year: 2018, text: 'Lorem Ipsum is simply dummy text', },
+  { year: 2019, text: 'Lorem Ipsum is simply dummy text', },
+  { year: 2020, text: 'Lorem Ipsum is simply dummy text', },
+  { year: 2021, text: 'Lorem Ipsum is simply dummy text', },
+  { year: 2022, text: 'Lorem Ipsum is simply dummy text', },
+];
